86382b59757f172848b51b83a467cdb825cb3002
Michael Hoffmann Erichsen <mhe@optimise.nu>
2026-02-01 12:44:38 +0100

    chore: version bump 1.0.2 (#83)

--- app.config.js
+++ app.config.js
@@ -5,9 +5,9 @@ module.exports = ({ config }) => {
   const appVariant = process.env.APP_VARIANT === 'dev' ? 'dev' : 'prod';
   const pickFirst = value => (Array.isArray(value) ? value[0] : value);
   const scheme = pickFirst(config.scheme) || 'footballcoach';
-  const expoVersion = '1.0.1';
-  const iosBuildNumber = '2';
-  const androidVersionCode = 2;
+  const expoVersion = '1.0.2';
+  const iosBuildNumber = '3';
+  const androidVersionCode = 3;
 
   return {
     ...config,
