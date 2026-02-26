5b1ac99f0ba2e330d2793953315d3f4b1583c391
Michael Hoffmann Erichsen <mhe@optimise.nu>
2026-02-26 14:35:34 +0100

    Set Home status bar icons back to dark

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1646,7 +1646,7 @@ export default function HomeScreen() {
       contextName={selectedContext?.name ?? undefined}
       contextType={adminTargetType || 'player'}
     >
-      <StatusBar barStyle="light-content" />
+      <StatusBar barStyle="dark-content" />
       
       {loading ? (
         <HomeSkeleton />
